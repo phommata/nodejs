b9987d996230eee4a614d7c19cce4dfa0dc81396
Andrew Phommathep <13destinies@gmail.com>
2016-04-03 19:35:20 -0700

    “Creating Your Own Shopping Cart”

---
+++ 28/cart_server.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+var db = mongoose.connect('mongodb://localhost/cart');
+require('./models/cart_model.js');
+var app = express();
+app.engine('.html', require('ejs').__express);
+app.set('views', __dirname + '/views');
+app.set('view engine', 'html');
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+require('./cart_routes')(app);
+// app.listen(80);
+app.listen(8000);
