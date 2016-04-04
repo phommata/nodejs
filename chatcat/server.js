53c677fc473f9302c695ce8b5e60cac3319d0c0e
Andrew Phommathep <13destinies@gmail.com>
2016-04-03 20:02:51 -0700

    32 Middleware

--- chatcat/server.js
+++ chatcat/server.js
@@ -3,14 +3,23 @@
 // console.log("Welcome to ChatCat");
 const express = require('express');
 const app = express();
+
 app.set('port', process.env.PORT || 3000);
+
+let helloMiddleware = (req, res, next) => {
+    req.hello = "Hello! It's me! I was wondering.....you get the idea!"
+    next();
+};
+
+app.use('/', helloMiddleware);
 
 app.get('/', (req,res, next) => {
     res.send('<h1>Hello Express!</h1>');
+    console.log(req.hello);
 });
 
 app.get('/dashboard', (req, res, next) => {
-    res.send('<h1>This is the dashboard page!</h1>');
+    res.send('<h1>This is the dashboard page! Middleware says: ' + req.hello +'</h1>');
 });
 
 app.listen(app.get('port'), () => {
