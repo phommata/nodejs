1f474a996ecadb2974f403bd6869f571421832f3
Andrew Phommathep <13destinies@gmail.com>
2016-04-04 21:04:01 -0700

    45. Implementing sessions using the express-session middleware

--- chatcat/app/routes/index.js
+++ chatcat/app/routes/index.js
@@ -12,6 +12,13 @@ module.exports = () => {
             },
             '/chat': (req, res, next) => {
                 res.render('chatroom');
+            },
+            '/getsession': (req, res, next) => {
+                res.send("My favourite color: " + req.session.favColor);
+            },
+            '/setsession': (req, res, next) => {
+                req.session.favColor = "Red";
+                res.send("Session Set");
             }
         },
         'post': {
