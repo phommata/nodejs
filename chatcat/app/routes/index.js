f2a8dac0039f94a5c7c429073d06d52fc35a0563
Andrew Phommathep <13destinies@gmail.com>
2016-04-04 21:41:30 -0700

    49 Social Authentication - Registering the app on Facebook

--- chatcat/app/routes/index.js
+++ chatcat/app/routes/index.js
@@ -12,13 +12,6 @@ module.exports = () => {
             },
             '/chat': (req, res, next) => {
                 res.render('chatroom');
-            },
-            '/getsession': (req, res, next) => {
-                res.send("My favourite color: " + req.session.favColor);
-            },
-            '/setsession': (req, res, next) => {
-                req.session.favColor = "Red";
-                res.send("Session Set");
             }
         },
         'post': {
