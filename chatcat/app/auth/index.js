41bbb63f09cefc46b44cf98ab5055c1e116efd5f
Andrew Phommathep <13destinies@gmail.com>
2016-04-04 22:05:10 -0700

    50. Social Authentication - Facebook Strategy

---
+++ chatcat/app/auth/index.js
@@ -0,0 +1,15 @@
+'use strict';
+const passport = require('passport');
+const config = require('../config');
+const h = require('../helpers');
+const FacebookStrategy = require('passport-facebook').Strategy;
+
+module.exports = () => {
+    let authProcessor = (accessToken, refreshToken, profile, done) => {
+        // Find a user in the local db using profile.id
+        // If the user is found, return the user data using the done()
+        // If the user is not found, create one in the local db and return
+    }
+
+    passport.use(new FacebookStrategy(config.fb, authProcessor));
+}
