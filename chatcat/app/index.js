a9a80af9166d7aa69c4e714b5274174ba053c703
Andrew Phommathep <13destinies@gmail.com>
2016-04-07 20:34:56 -0700

    60. Socket.io | Installation and rewiring the server

--- chatcat/app/index.js
+++ chatcat/app/index.js
@@ -3,7 +3,16 @@
 // Social Authentication Logic
 require('./auth')();
 
+// Create an IO Server instance
+let ioServer = app => {
+    const server = require('http').Server(app);
+    const io = require('socket.io').(server);
+    require('./socket')(io);
+    return server;
+}
+
 module.exports = {
     router: require('./routes')(),
-    session: require('./session')
+    session: require('./session'),
+    ioServer
 }
