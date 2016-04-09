3d1b5d4ab562eed1defd4213e9d216ea82e5b9e1
Andrew Phommathep <13destinies@gmail.com>
2016-04-09 00:16:12 -0700

    65. Socket.IO | Creating a new chatroom

--- chatcat/app/helpers/index.js
+++ chatcat/app/helpers/index.js
@@ -1,6 +1,7 @@
 'use strict';
 const router = require('express').Router();
 const db = require('../db');
+const crypto = require('crypto');
 
 // Iterate through the routes object and mount the routes
 let _registerRoutes = (routes, method) => {
@@ -73,11 +74,30 @@ let isAuthenticated = (req, res, next) => {
     }
 }
 
+// Find a chatroom by a given name
+let findRoomByName = (allrooms, room) => {
+    let findRoom = allrooms.findIndex((element, index, array) => {
+       if (element.room === room) {
+           return true;
+       } else {
+           return false;
+       }
+    });
+    return findRoom > -1 ? true: false;
+}
+
+// A function that generates a unique roomID
+let randomHex = () => {
+    return crypto.randomBytes(24).toString('hex');
+}
+
 module.exports = {
     // route: route
     route, // ES6 short-hand
     findOne,
     createNewUser,
     findById,
-    isAuthenticated
+    isAuthenticated,
+    findRoomByName,
+    randomHex
 }
--- chatcat/app/socket/index.js
+++ chatcat/app/socket/index.js
@@ -1,23 +1,30 @@
 'use strict';
+const h = require('../helpers');
 
 module.exports = (io, app) => {
     let allrooms = app.locals.chatrooms;
 
-    allrooms.push({
-        room: 'Good Food',
-        roomID: '0001',
-        users: []
-    });
-
-    allrooms.push({
-        room: 'Cloud Computing',
-        roomID: '0002',
-        users: []
-    });
-
     io.of('/roomslist').on('connection', socket => {
         socket.on('getChatrooms', () => {
             socket.emit('chatRoomsList', JSON.stringify(allrooms));
+        });
+
+        socket.on('createNewRoom', newRoomInput => {
+            // check to see if a room with the same title exists or not
+            // if not, create one and broadcast it to everyone
+            if (!h.findRoomByName(allrooms, newRoomInput)){
+                // Create a new room and broadcast to all
+                allrooms.push({
+                    room: newRoomInput,
+                    roomID: h.randomHex(),
+                    users: []
+                });
+
+                // Emit an updated list to the creator
+                socket.emit('chatRoomsList', JSON.stringify(allrooms));
+                // Emit an updated list to everyone connected to the rooms page
+                socket.broadcast.emit('chatRoomsList', JSON.stringify(allrooms));
+            }
         });
     })
 }
