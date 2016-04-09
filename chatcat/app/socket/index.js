d08423a165d4eb7f858d44dcce6ad2561bbf0722
Andrew Phommathep <13destinies@gmail.com>
2016-04-08 23:51:27 -0700

    64. Socket.IO | Rendering the list of chatrooms

--- chatcat/app/socket/index.js
+++ chatcat/app/socket/index.js
@@ -3,9 +3,21 @@
 module.exports = (io, app) => {
     let allrooms = app.locals.chatrooms;
 
-    io.of('/roomslist').on('connection', socket => {
+    allrooms.push({
+        room: 'Good Food',
+        roomID: '0001',
+        users: []
+    });
+
+    allrooms.push({
+        room: 'Cloud Computing',
+        roomID: '0002',
+        users: []
+    });
 
     io.of('/roomslist').on('connection', socket => {
-        console.log('Socket.io connected to client!');
+        socket.on('getChatrooms', () => {
+            socket.emit('chatRoomsList', JSON.stringify(allrooms));
+        });
     })
 }
