fd8cba31f4abf90575147dcf681f5d350bc8f463
Andrew Phommathep <13destinies@gmail.com>
2016-04-16 16:44:23 -0700

    93. Setting up Formidable for Receiving the files

--- photo-grid/app.js
+++ photo-grid/app.js
@@ -1,7 +1,10 @@
 var express = require('express'),
     path = require('path'),
     config = require('./config/config.js'),
-    knox = require('knox');
+    knox = require('knox'),
+    fs = require('fs'),
+    os = require('os'),
+    formidable = require('formidable');
 
 var app = express();
 
@@ -19,7 +22,7 @@ var knoxClient = knox.createClient({
     bucket:config.S3Bucket
 });
 
-require('./routes/routes.js')(express, app);
+require('./routes/routes.js')(express, app, formidable, fs, os);
 
 var server = require('http').createServer(app);
 var io = require('socket.io')(server);
--- photo-grid/routes/routes.js
+++ photo-grid/routes/routes.js
@@ -6,7 +6,35 @@ module.exports = function(express, app){
     })
 
     router.post('/upload', function(req, res, next){
-        // File upload
+        // File upload I
+
+        function generateFilename(filename) {
+            var ext_regex = /(?:\.([^.]+))?$/;
+            var ext = ext_regex.exec(filename)[1];
+            var date = new Date().getTime();
+            var charBank = "abcdefghijklmnopqrstuvwxyz";
+            var fstring = '';
+            for (var i = 0; i < 15; i++){
+                fstring += charBank[parseInt(Math.random()*26)];
+            }
+            return (fstring += date + '.' + ext);
+        }
+        var tmpFile, nfile, fname;
+        var newForm = new formidable.IncomingForm();
+            newForm.keepExtensions = true;
+            newForm.parse(req, function (err, fields, files) {
+                tmpFile = files.upload.path;
+                fname = generateFilename(files.upload.name);
+                nfile = os.tmpDir() + '/' + fname;
+                res.writeHead(200, {'Content-type:' 'text/plain'});
+                res.end();
+            })
+
+            newForm.on('end', function () {
+                fs.rename(tmpFile, nfile, function(){
+                    // Resize the image and upload this file into the S3 bucket
+                })
+            })
     });
 
     app.use('/', router);
