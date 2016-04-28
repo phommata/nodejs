c31213d2c6fb39a0071c0e15054b12c5986370cb
Andrew Phommathep <13destinies@gmail.com>
2016-04-27 17:13:40 -0700

    125. The 'Process' Button State

--- cards_printer/node/core/main/index.js
+++ cards_printer/node/core/main/index.js
@@ -6,6 +6,7 @@ function main(server, options, next) {
     const connectOpt = [process.env.appHost, process.env.appPort, process.env.appUser, process.env.appPass];
     const client = new Stomp(...connectOpt); // node --harmony-spreadcalls app.js <staging feature>
     const io = SocketIO(server.listener);
+    let itemArray = [];
 
     client.connect(sessionId =>{
         console.log('Connected to Apache Apollo');
@@ -13,6 +14,18 @@ function main(server, options, next) {
 
     io.on('connection', socket => {
         console.log('Connected!');
+
+        if (itemArray.length > 0){
+            // Keep the button disabled
+            socket.emit("buttonState", {
+                state: false
+            })
+        } else {
+            // Enable the button
+            socket.emit("buttonState", {
+                state: true
+            })
+        }
     });
 
     return next();
